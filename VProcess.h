d92880bf6e5c6df401ac6571e9024cf8f76accf4
EdmondBrowning <shizhan19960415@hotmail.com>
2018-06-11 21:38:05 +0800

    add VProcess

---
+++ VProcess.h
@@ -0,0 +1,12 @@
+//
+// Created by edmond on 18-6-11.
+//
+
+#ifndef VALENET_VPROCESS_H
+#define VALENET_VPROCESS_H
+namespace wily{
+    class VProcess{
+        
+    };
+}
+#endif //VALENET_VPROCESS_H
