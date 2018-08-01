7ab1041f552f5c4dae85532c64b713bf23889e8d
EdmondBrowning <shizhan19960415@hotmail.com>
2018-08-01 18:45:54 +0800

    change project path

---
+++ src/cmake-build-debug/CMakeFiles/ValeNet.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ValeNet.dir/VProcess.cpp.o"
+  "CMakeFiles/ValeNet.dir/VSocket.cpp.o"
+  "CMakeFiles/ValeNet.dir/Vale.cpp.o"
+  "ValeNet.pdb"
+  "ValeNet"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ValeNet.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
